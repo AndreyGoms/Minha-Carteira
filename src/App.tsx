a4ad5c5bcc2e5958aa71e7ead3dc2eae77d7c0f3
Andrey Gomes <andreygm74@gmail.com>
2024-03-01 20:26:00 -0300

    Desencolvimento da estrutura basica da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from "./styles/GlobalStyles";
+import dark from './styles/themes/dark'
+import light from './styles/themes/light'
+import Layout from "./Components/Layout";
 
-import Dashboard from "./pages/Dashboard";
 
 const App : React.FC = () => {
     return (
-        <>
+        <ThemeProvider theme={light}>
             <GlobalStyles/>
-            <Dashboard/>
-        </>
+            <Layout/>
+        </ThemeProvider>
     );
 }
 
