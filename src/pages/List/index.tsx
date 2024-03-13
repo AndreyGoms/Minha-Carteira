904b2f850e90b375035b6073d7aefa2cb2c75e9f
João Andrey Gomes Marques <joao.andrey@beansoftwares.com.br>
2024-03-13 00:00:05 -0300

    Criação do HistoryFinanceCard, que consiste em guardar o valor da conta para listar

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 import ContentHeader from "../../Components/ContentHeader";
 import SelectInput from "../../Components/SelectInput";
+import HistoryFinanceCard from "../../Components/HistoryFinanceCard";
 
 const List : React.FC = () => {
 
@@ -16,6 +17,16 @@ const List : React.FC = () => {
           <ContentHeader title="Saídas" lineColor="#E44C4E">
             <SelectInput options={options}/>
           </ContentHeader>
+
+          <Content>
+            <HistoryFinanceCard              
+              tagColor="#e44c4e"
+              title="conta de luz"
+              subtitle="27/10/2024"
+              amount="R$ 130,00"
+            />
+
+          </Content>
       </Container>
     );
 }
