d90be7c89b3fb148960f217af446eb66b11ddb28
João Andrey Gomes Marques <joao.andrey@beansoftwares.com.br>
2024-06-26 23:51:30 -0300

    Fix: pequeno teste com componente messageBox

--- src/Components/MessageBox/index.tsx
+++ src/Components/MessageBox/index.tsx
@@ -9,23 +9,41 @@ interface IMessageBoxProps {
     icon: string;
 }
 
-const MessageBox : React.FC<IMessageBoxProps> = ({
-    title,
-    description,
-    footerText,
-    icon}
-) => {
+// const MessageBox : React.FC<IMessageBoxProps> = ({
+//     title,
+//     description,
+//     footerText,
+//     icon}
+// ) => {
+//     return (
+//         <Container>
+//            <header>
+//                 <h1>
+//                     {title} <img src={icon} alt={title}/>
+//                 </h1>
+//                 <p>{description}</p>
+//            </header>
+
+//            <footer>
+//                  <span>{footerText}</span>
+//            </footer> 
+//         </Container>
+//     );
+// }
+
+const MessageBox = (props:IMessageBoxProps) => {
+    
     return (
         <Container>
            <header>
                 <h1>
-                    {title} <img src={icon} alt={title}/>
+                    {props.title} <img src={props.icon} alt={props.title}/>
                 </h1>
-                <p>{description}</p>
+                <p>{props.description}</p>
            </header>
 
            <footer>
-                 <span>{footerText}</span>
+                 <span>{props.footerText}</span>
            </footer> 
         </Container>
     );
